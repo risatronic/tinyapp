c8a1f75e22ead54cfb56403eb60a6ff2c2b5134d
Marisa Doig <mcr.doig@gmail.com>
2020-08-03 21:25:30 +0000

    Added body-parser and a functino to generate a 6 character random string

--- express_server.js
+++ express_server.js
@@ -4,6 +4,13 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -12,6 +19,11 @@ const urlDatabase = {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {
