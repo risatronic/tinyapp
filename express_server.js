b455c5d05364ed5f7577427048744694f96acc13
Marisa Doig <mcr.doig@gmail.com>
2020-08-03 21:48:32 +0000

    Added functionality to make Post request add shortURL-longURL pair to urlDatabase object

--- express_server.js
+++ express_server.js
@@ -22,10 +22,10 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  urlDatabase[generateRandomString()] = req.body.longURL;
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
   console.log(urlDatabase);
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -34,7 +34,7 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
-  let longURL = urlDatabase[req.params.shortURL]
+  let longURL = urlDatabase[req.params.shortURL];
   let templateVars = { shortURL, longURL  };
   res.render("urls_show", templateVars);
 });
